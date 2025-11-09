0e68d3a4447f5a8c44f48e5c41b772a31e153af5
MatHenriquez <matias.henriquez.dev@gmail.com>
2025-11-09 15:12:16 -0300

    refactor: update import paths and improve routing structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import "./App.css";
-import CountryList from "./components/CountryList";
-import CountryDetails from "./components/CountryDetails";
-import About from "./components/About";
-import NotFound from "./components/NotFound";
+import CountryList from "./features/country-list/CountryList";
+import CountryDetails from "./features/country-detail/CountryDetails";
+import About from "./features/about/About";
+import NotFound from "./features/not-found/NotFound";
+import { PaginationProvider } from "./features/country-list/context/PaginationContext";
+import { ROUTES } from "./api/routes";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-
+/**
+ * Componente principal de la aplicación
+ * @returns {JSX.Element} - Componente App
+ */
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-brand">
-            🌍 Countries Explorer
-          </Link>
-          <div className="navbar-links">
-            <Link to="/" className="navbar-link">
-              Inicio
+      <PaginationProvider>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to={ROUTES.HOME} className="navbar-brand">
+              🌍 Countries Explorer
             </Link>
-            <Link to="/about" className="navbar-link">
-              Acerca de
-            </Link>
+            <div className="navbar-links">
+              <Link to={ROUTES.HOME} className="navbar-link">
+                Inicio
+              </Link>
+              <Link to={ROUTES.ABOUT} className="navbar-link">
+                Acerca de
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<CountryList />} />
-        <Route path="/CountryInfo/:name" element={<CountryDetails />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes> 
+        </nav>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<CountryList />} />
+          <Route path={ROUTES.COUNTRY_INFO} element={<CountryDetails />} />
+          <Route path={ROUTES.ABOUT} element={<About />} />
+          <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+        </Routes>
+      </PaginationProvider>
     </BrowserRouter>
   );
 }
